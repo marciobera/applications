1e51a7cc14147de8f89027e6ba6989a83cee5146
Marcio Bera <marciobera@hotmail.com>
2017-11-25 20:43:35 -0200

    Using cache and angular cookies.

--- powerevents/app/scripts/controllers/main.js
+++ powerevents/app/scripts/controllers/main.js
@@ -8,9 +8,11 @@
  * Controller of the eventsApp
  */
 angular.module('eventsApp')
-  .controller('MainCtrl', function ($http, $sce, $location, $rootScope, $cacheFactory) {
+  .controller('MainCtrl', function ($http, $sce, $location, $rootScope, $cacheFactory, $cookies) {
   	var main = this;
     main.loading = false;
+    main.cookies = $cookies;
+    main.lastArtists = [];
 
     var cache = $cacheFactory('myCache');
 
@@ -36,6 +38,9 @@ angular.module('eventsApp')
     // methods
 
     main.clearSearch = function(name) {
+
+      main._lastArtists();
+
       if(name){
         main.artist.name = '';
       }
@@ -53,7 +58,9 @@ angular.module('eventsApp')
     }
 
     main.getArtist = function(){
+
       main.loading = true;
+
       main.clearSearch(0);
       if(!main.artist.name){
         main.clearSearch(1);
@@ -61,33 +68,33 @@ angular.module('eventsApp')
         return false;
       }
 
-      var _getCache = cache.get(main.artist.name);
+      var _cacheArtist = $cookies.getObject(main.artist.name);
 
-      if(_getCache){
-
-        main.artist.content = _getCache;
+      if(_cacheArtist){
+        main.artist.content = _cacheArtist;
         main.artist.content.facebook_iframe_url = main._getFacebookPageUrl(main.artist.content.facebook_page_url);
         main.loading = false;
-        return false;
 
+        return false;
       }
 
     	$http.get(url + main.artist.name, config).
     	then(function(response){
 
-        cache.put(main.artist.name, response.data);
-        console.log(response);
         main.loading = false;
         main.artist.content = response.data;
         main.artist.content.facebook_iframe_url = main._getFacebookPageUrl(main.artist.content.facebook_page_url);
 
+        $cookies.putObject(main.artist.name, response.data);
+
     	},function (error){
-        console.log(error);
+        // console.log(error);
         main.loading = false;
         main.artist.error.code = 1;
         main.artist.error.type = 'danger';
         main.artist.error.message = '<strong>Sorry</strong>, <em>' + main.artist.name + '</em> was not found. :(';
       });
+
     }
 
     main.getNextEvents = function(){
@@ -104,31 +111,36 @@ angular.module('eventsApp')
         return false;
       }
 
-      var _getCache = cache.get(main.artist.name + "/events");
+      var _getCache = cache.get(main.artist.name);
 
       if(_getCache){
-        main.artist.events = _getCache;
+        main.artist.events = _getCache.events;
         main.loading = false;
         return false;
       }
 
       $http.get(url + main.artist.name + "/events", config)
       .then(function(response){
+        console.log(response);
         
         main.loading = false;
-        cache.put(main.artist.name + "/events", response.data);
-
+        
         if(response.data.length > 0){
+          
           main.artist.events = response.data;
+          cache.put(main.artist.name, main.artist);
+
         }else{
           main.artist.error.code = 1;
           main.artist.error.type = 'warning';
           main.artist.error.message = '<strong>Sorry</strong>, no events were found for this artist. :(';
         }
-        
+
+
+
       },
       function(error){
-        console.log(error);
+        // console.log(error);
         main.loading = false;
         main.artist.error.code = 1;
         main.artist.error.type = 'warning';
@@ -143,6 +155,22 @@ angular.module('eventsApp')
             "https://www.facebook.com/plugins/like.php?href=" + main.artist.content.facebook_page_url + "&width=300&layout=standard&action=like&size=small&show_faces=false&share=false&height=40&appId=433250860343994&colorscheme=dark"
             );
       }
+    }
+
+    main._lastArtists = function(){
+
+      main.lastArtists = [];
+      var itens = main.cookies.getAll();
+      delete itens._ga;
+      delete itens._gid;
+      delete itens._gat;
+
+      angular.forEach(itens, function(item, key){
+        item = JSON.parse(item);
+        item.input = key;
+        main.lastArtists.push(item);
+        
+      });
     }
 
     main.clearSearch(0);
