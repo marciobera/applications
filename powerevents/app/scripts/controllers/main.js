7b3c8fdf1961373bbea68906a4d8a645a1929cee
Marcio Bera <marciobera@hotmail.com>
2017-11-25 18:30:20 -0200

    Setting $http cache for both requisitions.

--- powerevents/app/scripts/controllers/main.js
+++ powerevents/app/scripts/controllers/main.js
@@ -8,9 +8,11 @@
  * Controller of the eventsApp
  */
 angular.module('eventsApp')
-  .controller('MainCtrl', function ($http, $sce, $location, $rootScope) {
+  .controller('MainCtrl', function ($http, $sce, $location, $rootScope, $cacheFactory) {
   	var main = this;
     main.loading = false;
+
+    var cache = $cacheFactory('myCache');
 
     main.artist = {name: ''};
 
@@ -27,7 +29,8 @@ angular.module('eventsApp')
     var config = {
             params:{
              "app_id":"vanhack_challange"
-            }
+            },
+            cache: cache
         };
 
     // methods
@@ -57,17 +60,29 @@ angular.module('eventsApp')
         main.loading = false;
         return false;
       }
+
+      var _getCache = cache.get(main.artist.name);
+
+      if(_getCache){
+
+        main.artist.content = _getCache;
+        main.artist.content.facebook_iframe_url = main._getFacebookPageUrl(main.artist.content.facebook_page_url);
+        main.loading = false;
+        return false;
+
+      }
+
     	$http.get(url + main.artist.name, config).
     	then(function(response){
+
+        cache.put(main.artist.name, response.data);
         console.log(response);
         main.loading = false;
         main.artist.content = response.data;
-        if(main.artist.content.facebook_page_url){
-          main.artist.content.facebook_iframe_url = $sce.trustAsResourceUrl(
-            "https://www.facebook.com/plugins/like.php?href=" + main.artist.content.facebook_page_url + "&width=300&layout=standard&action=like&size=small&show_faces=false&share=false&height=40&appId=433250860343994&colorscheme=dark"
-            );
-        }
+        main.artist.content.facebook_iframe_url = main._getFacebookPageUrl(main.artist.content.facebook_page_url);
+
     	},function (error){
+        console.log(error);
         main.loading = false;
         main.artist.error.code = 1;
         main.artist.error.type = 'danger';
@@ -76,6 +91,9 @@ angular.module('eventsApp')
     }
 
     main.getNextEvents = function(){
+
+      
+
       main.loading = true;
       main.artist.events = [];
       if(!main.artist.name){
@@ -85,10 +103,21 @@ angular.module('eventsApp')
         main.loading = false;
         return false;
       }
-      $http.get(url + main.artist.name + "/events", config).
-      then(function(response){
-        console.log(response);
+
+      var _getCache = cache.get(main.artist.name + "/events");
+
+      if(_getCache){
+        main.artist.events = _getCache;
         main.loading = false;
+        return false;
+      }
+
+      $http.get(url + main.artist.name + "/events", config)
+      .then(function(response){
+        
+        main.loading = false;
+        cache.put(main.artist.name + "/events", response.data);
+
         if(response.data.length > 0){
           main.artist.events = response.data;
         }else{
@@ -96,7 +125,24 @@ angular.module('eventsApp')
           main.artist.error.type = 'warning';
           main.artist.error.message = '<strong>Sorry</strong>, no events were found for this artist. :(';
         }
+        
+      },
+      function(error){
+        console.log(error);
+        main.loading = false;
+        main.artist.error.code = 1;
+        main.artist.error.type = 'warning';
+        main.artist.error.message = '<strong>Sorry</strong>, no events were found for this artist. :(';
       });
+
+    }
+
+    main._getFacebookPageUrl = function(url){
+      if(url){
+        return $sce.trustAsResourceUrl(
+            "https://www.facebook.com/plugins/like.php?href=" + main.artist.content.facebook_page_url + "&width=300&layout=standard&action=like&size=small&show_faces=false&share=false&height=40&appId=433250860343994&colorscheme=dark"
+            );
+      }
     }
 
     main.clearSearch(0);
