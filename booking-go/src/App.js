337feb80fa35feed803c896e690e6f6a2c656d3d
Marcio Bera <marciobera@hotmail.com>
2019-03-05 00:30:30 +0000

    Including mainly parts of form box according with the rentalcars.com design.

--- booking-go/src/App.js
+++ booking-go/src/App.js
@@ -6,6 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="form">
+            <form>
+              <h2 className="title">Let’s find your ideal car</h2>
+              <label htmlFor="search" title="Pick Up Location">Pick-up Location</label>
+              <input id="search" type="text" placeholder="city, airport, station, region, district..." />
+            </form>
+          </div>
         </header>
       </div>
     );
